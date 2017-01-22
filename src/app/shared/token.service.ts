ca5e22276727d92c5a172d7c9edabcf9ba2733ce
johnpmitsch <johnpmitsch@gmail.com>
2017-01-22 10:05:34 -0500

    debugging production

--- src/app/shared/token.service.ts
+++ src/app/shared/token.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Angular2TokenService } from 'angular2-token';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class TokenService {
   constructor(public _tokenService: Angular2TokenService) {
-    console.log(process.env.ENV);
-    if (process.env.ENV === 'production') {
-      var apiPath = "https://geekbread-server.herokuapp.com/"
-    } else {
-      var apiPath = 'http://localhost:3000'
-    }
-
 	  this._tokenService.init({
-      apiPath:                    apiPath,
+      apiPath:                    environment.apiPath,
 			signInPath:                 'auth/sign_in',
 			signInRedirect:             'sign-in',
 			signInStoredUrlStorageKey:  null,
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 
-if (process.env.ENV === 'production') {
+if (process.env.ENV === 'production' || process.env.NODE_ENV === 'production') {
     enableProdMode();
 }
 
