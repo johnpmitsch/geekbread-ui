4735c9ab97ee471abe5b6ea79d8743af8a7fe25d
johnpmitsch <johnpmitsch@gmail.com>
2017-06-04 20:19:06 -0400

    updated path

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 
 if (process.env.ENV === 'production') {
       enableProdMode();
